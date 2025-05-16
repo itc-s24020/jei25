1ec2b30094494b1fa4ab3f056325b630adf38318
itc-s24020 <s24020@std.it-college.ac.jp>
2025-05-16 12:34:21 +0900

    課題

---
+++ D48Q25.java
@@ -0,0 +1,10 @@
+public class D48Q25 {
+    String codo ;
+    public static void main(String[] args) {
+        D48Q25 obj = new D48Q25();
+        obj.setCodo("FB1257");
+    }
+    void setCodo(String s) {
+        codo = s;
+    }
+}
