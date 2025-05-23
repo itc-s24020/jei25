47987feaa6a76c2134c0563efd3a3a59118d5c5c
itc-s24020 <s24020@std.it-college.ac.jp>
2025-05-23 11:39:49 +0900

    課題

---
+++ D49Q20kadai.java
@@ -0,0 +1,11 @@
+public class D49Q20kadai {
+    public static void main(String[] args) {
+        double[][] arr = new double[5][10];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
