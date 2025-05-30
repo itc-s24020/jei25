ccedffcd32de9b603cbd6887145c71e5baed287b
itc-s24020 <s24020@std.it-college.ac.jp>
2025-05-30 12:28:10 +0900

    課題

---
+++ D50Q27kadai.java
@@ -0,0 +1,17 @@
+public class D50Q27kadai {
+    public static void main(String[] args) {
+        int control = 1;
+
+        switch (control) {
+            case 1:
+                System.out.println("Warm");
+                break;
+            case 2:
+                System.out.println("Cool");
+                break;
+            case 3:
+                System.out.println("Wind");
+                break;
+        }
+    }
+}
