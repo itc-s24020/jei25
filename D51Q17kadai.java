562b32d6059cd63282efd182f932a029375b523f
itc-s24020 <s24020@std.it-college.ac.jp>
2025-06-13 12:34:25 +0900

    課題

---
+++ D51Q17kadai.java
@@ -0,0 +1,13 @@
+public class D51Q17kadai {
+    public static void main(String[] args) {
+        int x = 4;
+        if (x > 4) {
+            System.out.println("over 4");
+        } else if (x == 4) {
+            System.out.println("just 4");
+        } else if (x < 4) {
+            System.out.println("under 4");
+        }
+    }
+
+}
