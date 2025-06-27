dae51db03bd4866390360ed21db7b8044f013f92
itc-s24020 <s24020@std.it-college.ac.jp>
2025-06-27 12:28:22 +0900

    課題

---
+++ D51Q23kadai.java
@@ -0,0 +1,8 @@
+public class D51Q23kadai {
+    public static void main(String[] args) {
+        String[] kisetu = {"春", "夏", "秋", "冬"};
+        for (String a : kisetu) {
+            System.out.print(a + "。");
+        }
+    }
+}
