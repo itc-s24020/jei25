51993164990fa0a156cfe900a49dd713742a351c
itc-s24020 <s24020@std.it-college.ac.jp>
2025-06-27 10:57:50 +0900

    課題

---
+++ D51Q19kadai.java
@@ -0,0 +1,11 @@
+import java.awt.image.BandedSampleModel;
+
+public class D51Q19kadai {
+    public static void main(String[] args) {
+        float f = 4.7f;
+        double d = 0.86;
+        double ans = f*d;
+        System.out.println(ans);
+    }
+}
+
