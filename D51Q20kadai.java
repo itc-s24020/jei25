850488077bfe9261f05b4aa36b4fcf984f9a212b
itc-s24020 <s24020@std.it-college.ac.jp>
2025-06-27 11:30:10 +0900

    課題

---
+++ D51Q20kadai.java
@@ -0,0 +1,8 @@
+public class D51Q20kadai {
+    public static void main(String[] args) {
+        int n = 85;
+        String c = (n > 75) ? "合格" : "不合格";
+        System.out.println(c);
+    }
+}
+
